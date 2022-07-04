9d62217bf74eea9ae6f493b8e3cd23c695514cc8
Nadusha <nadusha230275@gmail.com>
2022-07-04 09:22:48 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// показать четные числа от 1 до n
+Console.WriteLine("введите число ");
+int n=int.Parse(Console.ReadLine());
+int res=1;
+while (res<n)
+
+{
+    Console.Write(res+1);
+    Console.WriteLine("  чётное число ");
+res+=2;
+}
